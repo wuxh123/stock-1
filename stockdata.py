3a5cb95a3a5c27085792131184748bdad1dae7a7
zdevt <zhangtao.hawk@qq.com>
2019-09-12 12:40:49 +0800

    update

--- stockdata.py
+++ stockdata.py
@@ -6,7 +6,7 @@
 #
 #        Version:  1.0
 #        Created:  2019-06-18 16:07:49
-#  Last Modified:  2019-09-11 17:55:43
+#  Last Modified:  2019-09-12 12:40:38
 #       Revision:  none
 #       Compiler:  gcc
 #
@@ -27,9 +27,11 @@ class stockdata:
         with open('tk.pkl', 'rb') as f:
             tk = pickle.load(f)
             ts.set_token(tk)
+        # 起始日期
+        self.st_date = "20140101"
         self.pro = ts.pro_api()
         # 原始数据存数据库0
-        self.r0 = redis.Redis(host='127.0.0.1', password='zt@123456', port=6379, db=0)
+        self.r0 = redis.Redis(host='192.168.0.188', password='zt@123456', port=6379, db=0)
         # 加工后的数据存数据库1
         self.r1 = redis.Redis(host='127.0.0.1', password='zt@123456', port=6379, db=1)
 
@@ -93,15 +95,20 @@ class stockdata:
         data = self.pro.hk_hold(trade_date=date)
         print(data)
 
-    def delx(self):
-        ds_date = self.get_trade_cal('2014101')
+    def check_all_data(self):
+        ds_date = self.get_trade_cal(self.st_date)
         ds_date = ds_date['cal_date']
         ds_date = ds_date.reset_index(drop=True)
 
         for i in ds_date.index:
             d = ds_date.loc[i]
-            self.r0.hdel(d, '399006.SH')
-            self.r0.hdel(d, '399001.SH')
+            dk = self.r0.hkeys(d)
+            for f in dk:
+                fs = f.decode()
+                data = pickle.loads(zlib.decompress(self.r0.hget(d, fs)))
+                if data.empty is True:
+                    print("del empty: ", d, fs)
+                    self.r0.hdel(d, fs)
 
     # ********************************************************************
 
@@ -144,14 +151,7 @@ class stockdata:
                 time.sleep(0.15)
 
     def get_all_data_save(self):
-        ds = "20140101"
-        '''
-        rd = self.r0.keys("20*")
-        if rd:
-            rd.sort()
-            ds = rd[-1].decode()
-        '''
-        ds_date = self.get_trade_cal(ds)
+        ds_date = self.get_trade_cal(self.st_date)
         ds_date = ds_date['cal_date']
         ds_date = ds_date.reset_index(drop=True)
 
@@ -176,18 +176,7 @@ if __name__ == '__main__':
     startTime = datetime.datetime.now()
 
     A = stockdata()
-    A.get_all_data_save()
+    # A.get_all_data_save()
+    A.check_all_data()
 
     print("Time taken:", datetime.datetime.now() - startTime)
-
-# d = A.get_top_inst_save('20190906')
-# d = A.get_stock_number_date_last_ndays("600818.SH", "20180101", 40)
-# d = A.get_index_daily_start_end_date_save_all("20190906")
-# A.delx()
-# A.test('20190719')
-# A.get_index_daily_save('399001.SZ', '20150105')
-# d = A.get_index_daily_start_end_date_save("000001.SH", "20190909")
-# A.get_one_day_data_save('20190906')
-# d = A.get_date_limitup('20190909')
-# d = A.get_trade_cal("20190901")
-# print(d)
